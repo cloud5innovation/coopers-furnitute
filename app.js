035ff8da580bb16af261bc21baaf8b90df9bec58
Latifah <latifah.pres@gmail.com>
2020-05-20 15:14:40 -0500

    server running and travis-ci set up

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const app = express();
+
+app.use(express.json());
+//TODO: SHOULD MORGAN LOGS BE UPLOADED TO HEORKU OR KEPT FOR DEVELOPMENT
+// app.use(morgan('combined', {stream: accessLogStream}));
+app.use(helmet());
+app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.send('sanity check!')
+});
+
+module.exports = app
