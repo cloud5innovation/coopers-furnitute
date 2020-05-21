f4f19fe0d64442b79cfefb2e9c7fe9930fe5cc45
Latifah <latifah.pres@gmail.com>
2020-05-21 18:16:48 -0500

    Deployed to heroku

--- knexfile.js
+++ knexfile.js
@@ -50,19 +50,19 @@ module.exports = {
     }
   },
 
-  // production: {
-  //   client: 'postgresql',
-  //   connection: process.env.DATABASE_URL,
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     directory: './database/migrations'
-  //   },
-  //   seeds: {
-  //     directory: './database/seeds'
-  //   },
-  // }
+  production: {
+    client: 'pg',
+    connection: process.env.PROD_DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './database/migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
+    },
+  }
 
 };
