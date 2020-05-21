3ff8bd3d0efd7507f34d97d961644e4973d7c37e
Latifah <latifah.pres@gmail.com>
2020-05-21 00:55:49 -0500

    created products model

---
+++ models/products.js
@@ -0,0 +1,36 @@
+const db = require('../dbconfig.js');
+
+products = () => {
+    return db('products').select('id', 'title', 'price', 'description', 'image_url', 'category', 'quantity', 'item_number', 'supplier')
+};
+
+productById = (id) => {
+    return db('products').where({'id': id}).first()
+};
+
+addProduct = (product) => {
+    return db('products').insert(product)
+};
+
+editProduct = (id, product) => {
+    return db('products').where({ id }).update(product)
+}
+
+deleteProduct = (id) => {
+    return db('products').where({ id }).del()
+}
+
+filterBy = (col, filter) => {
+    // let items  = {
+    //     col, filter
+    // }
+    return db('products').where(`${col}`, filter)
+}
+module.exports = {
+    products,
+    productById,
+    addProduct,
+    editProduct,
+    deleteProduct,
+    filterBy
+}
